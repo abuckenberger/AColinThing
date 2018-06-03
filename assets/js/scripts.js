25fe6a1939c7527ee3d2e93f402a755f299054d9
abuckenberger <abuckenberger@eagles.ewu.edu>
2018-06-03 16:50:38 -0700

    p5 bg color
    * added p5
    * hope it works hahaha

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,2 +1,27 @@
 // Your Scripts
 console.log('Hey Browser!');
+
+var words = ['hey', 'whats up', 'hello', 'hola', 'yo'];
+var colors = [
+  [63, 184, 175],
+  [127, 199, 175],
+  [218, 216, 167],
+  [255, 158, 157],
+  [255, 61, 127],
+];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  textAlign(CENTER, CENTER);
+  frameRate(3);
+}
+
+function draw() {
+  var currentIndex = frameCount % colors.length;
+  var currentColor = colors[currentIndex];
+  var currentWord = words[currentIndex];
+  background(currentColor);
+  fill(255);
+  textSize(45);
+  text(currentWord, width / 2, height / 2);
+}
